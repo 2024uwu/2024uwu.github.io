3087624be838fef04fd9a822aa8c9066f9aef321
Unbreakable When United <160858414+2024uwu@users.noreply.github.com>
2024-03-05 15:14:17 +0700

    Add files via upload

---
+++ members-files/members.js
@@ -0,0 +1,19 @@
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector(".navbutton");
+
+console.log(darkMode);
+const enableDarkMode = () => {
+    document.body.classList.add("darkMode");
+    localStorage.setItem("darkMode","enabled");
+}
+
+const disableDarkmode =() =>{
+    document.body.classList.remove("darkMode");
+    localStorage.setItem("darkMode", null);
+
+}
+
+if(darkMode === "enabled"){
+    enableDarkMode();
+}
+
