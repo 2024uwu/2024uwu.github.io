0ab48e3cb9ab7b56f67dc4249d002ec61800eb05
UwU <austinkurniawan01@gmail.com>
2024-04-14 14:33:47 +0700

    Format files

--- members-files/members.js
+++ members-files/members.js
@@ -3,17 +3,15 @@ const darkModeToggle = document.querySelector(".navbutton");
 
 console.log(darkMode);
 const enableDarkMode = () => {
-    document.body.classList.add("darkMode");
-    localStorage.setItem("darkMode","enabled");
+  document.body.classList.add("darkMode");
+  localStorage.setItem("darkMode", "enabled");
+};
+
+const disableDarkmode = () => {
+  document.body.classList.remove("darkMode");
+  localStorage.setItem("darkMode", null);
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode();
 }
-
-const disableDarkmode =() =>{
-    document.body.classList.remove("darkMode");
-    localStorage.setItem("darkMode", null);
-
-}
-
-if(darkMode === "enabled"){
-    enableDarkMode();
-}
-
